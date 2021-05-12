f6a73515062deb0e60f9cf92073c9314487c4da3
ArchieF-T <76113533+ArchieF-T@users.noreply.github.com>
2021-05-12 09:47:52 +0100

    Create 3-1 IF statements.py
    Using IF statements in Python

---
+++ 3-1 IF statements.py
@@ -0,0 +1,14 @@
+#Archie Fall-Taylor 05/02/2021
+#Using IF... Statements
+
+#Display splash message
+print("***Welcome to H. Wensleydale Cheese Emporium***")
+print("Would you like some cheese?")
+
+#Get user input
+wantsCheese = input("Enter (y)es or (n)o: ")
+
+if wantsCheese == "y":
+    print("I'm sorry, we don't have any cheese.")
+if wantsCheese == "n":
+    print("That's good, we don't have any.")
